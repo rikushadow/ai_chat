e298d939aa74ba9d47c537d0079ccffc28fd5b90
rikushadow <rikushadow2@gmail.com>
2025-10-18 14:52:12 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def chat():
     return jsonify({"reply": response.choices[0].message.content})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
